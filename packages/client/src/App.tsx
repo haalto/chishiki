92f54843ec77359e6f3a6cd45f4865d4fcb2bb93
Hannes A <aaltonen.hannes@gmail.com>
2020-12-28 00:46:33 +0200

    added styling for landing page

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -3,16 +3,39 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Landing from "./views/Landing";
 import Game from "./views/Game";
 import Play from "./views/Play";
+import { createGlobalStyle } from "styled-components";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+interface StyleProps {
+  darkMode: boolean;
+}
+
+const GlobalStyle = createGlobalStyle<StyleProps>`
+  html {
+    min-height: 100%;
+  }
+
+  body {
+    font-family: 'Carter One', cursive;
+    //background-color: ${(props) => (props.darkMode ? "black" : "yellow")};
+    background-color: #6EC5B8;
+  }
+`;
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/play/:roomCode/:username" component={Play} />
-        <Route path="/game" component={Game} />
-        <Route path="/" component={Landing} />
-      </Switch>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/play/:roomCode/:username" component={Play} />
+          <Route path="/game" component={Game} />
+          <Route path="/" component={Landing} />
+        </Switch>
+      </BrowserRouter>
+      <ToastContainer />
+      <GlobalStyle darkMode={false} />
+    </>
   );
 }
 
--- packages/client/src/views/Landing/index.tsx
+++ packages/client/src/views/Landing/index.tsx
@@ -1,23 +1,120 @@
 import React, { useState } from "react";
 
 import useNavigation from "../../hooks/useNavigation";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-items: center;
+  min-height: 100%;
+  margin-top: 3rem;
+`;
+
+const Button = styled.button`
+  padding: 0.5rem;
+  border: none;
+  margin: 0.2rem;
+  cursor: pointer;
+  color: white;
+  font-size: 1.1rem;
+  border-radius: 20px;
+  font-family: inherit;
+  font-weight: 50;
+`;
+
+const ButtonJoin = styled(Button)`
+  width: 6rem;
+  margin-top: 4rem;
+  background: #f7971e; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to left,
+    #ffd200,
+    #f7971e
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to left,
+    #ffd200,
+    #f7971e
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+`;
+
+const ButtonCreate = styled(Button)`
+  margin-top: 13rem;
+  background: none;
+  border: none;
+  color: lightgrey;
+  :hover {
+    border-bottom: 2px solid transparent;
+    outline: none;
+    border-image-slice: 1;
+    border-image-source: linear-gradient(to left, #ffd200, #f7971e);
+  }
+`;
+const Input = styled.input`
+  width: 10rem;
+  margin-top: 2rem;
+  margin-bottom: 0.3rem;
+  font-family: inherit;
+  background: none;
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  border-bottom: 1px solid lightgrey;
+  box-shadow: none;
+
+  ::placeholder {
+    /* Chrome, Firefox, Opera, Safari 10.1+ */
+    color: lightgrey;
+    opacity: 1; /* Firefox */
+  }
+
+  :focus {
+    border-bottom: 2px solid transparent;
+    outline: none;
+    border-image-slice: 1;
+    border-image-source: linear-gradient(to left, #ffd200, #f7971e);
+  }
+`;
 
 const Landing: React.FC = () => {
   const [roomCode, setRoomCode] = useState<string>("");
   const [username, setUsername] = useState<string>("");
   const { goToCreateNewGame, goToJoinGame } = useNavigation();
 
+  const handleRoomCode = (value: string) => {
+    if (value.length < 6) {
+      setRoomCode(value.toUpperCase());
+    }
+  };
+
+  const handleUsername = (value: string) => {
+    if (value.length < 15) {
+      setUsername(value.toUpperCase());
+    }
+  };
+
   return (
-    <div>
-      <div>
-        <label>room code</label>
-        <input value={roomCode} onChange={(e) => setRoomCode(e.target.value)} />
-        <label>username</label>
-        <input value={username} onChange={(e) => setUsername(e.target.value)} />
-        <button onClick={() => goToJoinGame(roomCode, username)}>Join</button>
-      </div>
-      <button onClick={() => goToCreateNewGame()}>Create game room</button>
-    </div>
+    <Wrapper>
+      <Input
+        placeholder={"ROOM CODE"}
+        value={roomCode}
+        onChange={(e) => handleRoomCode(e.target.value)}
+      />
+
+      <Input
+        placeholder={"USER NAME"}
+        value={username}
+        onChange={(e) => handleUsername(e.target.value)}
+      />
+      <ButtonJoin onClick={() => goToJoinGame(roomCode, username)}>
+        Join
+      </ButtonJoin>
+      <ButtonCreate onClick={() => goToCreateNewGame()}>
+        Create game room
+      </ButtonCreate>
+    </Wrapper>
   );
 };
 
